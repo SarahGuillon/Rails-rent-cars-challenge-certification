a31f9170d110cd9d8b7935d82977d22a6f640203
Sarah Guillon <sarah.guillon.11@gmail.com>
2022-03-24 16:32:50 +0100

    controller Cars

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,5 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_many :bookings, dependent: :destroy
-  has_many :cars, through: :bookings
   has_many :cars, dependent: :destroy
 end
