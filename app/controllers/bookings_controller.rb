4e9b45d7d348a30b29cf6951fe373d32e2996cd0
Sarah Guillon <sarah.guillon.11@gmail.com>
2022-03-25 12:08:03 +0100

    front end des notifications

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -11,12 +11,18 @@ class BookingsController < ApplicationController
     if @booking.starts_at.nil? || @booking.ends_at.nil?
       return redirect_to car_path(@car), notice: "Dates are incorrect"
     end
+    if @booking.starts_at > @booking.ends_at
+      return redirect_to car_path(@car), notice: "End date should be superior at start date "
+    end
+    if @booking.starts_at < DateTime.now
+      return redirect_to car_path(@car), notice: "Date must be greater than today's date"
+    end
 
     @booking.car = @car
     @booking.user = current_user
     @booking.accepted = true
     if @booking.save
-      redirect_to car_path(@car), notice: "Your booking is confirmed"
+      redirect_to car_path(@car), notice: "Your booking is confirmed ✅"
     else
       render "cars/show"
     end
